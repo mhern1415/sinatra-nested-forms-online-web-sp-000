967820dbd26683a7bfd9984a045ba9bb05d6aa99
Mark Hernandez <m.hern1415@gmail.com>
2019-10-14 22:54:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,18 @@ module FormsLab
     erb :'pirates/new'
     
     end
+    
+    post '/pirates' do
+      params["pirate"]["ships"].each do |ship_data|
+        Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
+      end
+
+      @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
 
   end
 end
